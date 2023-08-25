7367256cf5164c1c8a2d6abaca1f72740988165e
Rahil Khan <rahil4rhail@gmail.com>
2023-08-26 02:49:40 +0530

    Update and rename testscript3.js to testscript4.js

--- testscript3.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  console.log("My Name is Rahil") 
-  alert("Hello")
-  
-})
---
+++ testscript4.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log("My Name is Rahil")
+  alert("Deki Suki")
+});
